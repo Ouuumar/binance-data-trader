73afdf5dd05508e2498a807418dea1b90e04b8fd
Ouuumar <sk8pro@hotmail.fr>
2023-10-17 19:24:33 +0200

    chore(etl): simplify etl behavior

--- etl/init_data.py
+++ etl/init_data.py
@@ -1,9 +1,18 @@
 import time
-from functions import *
 from binance import Client
+import pathlib
+import sys 
+from dotenv import load_dotenv
+from functions import *
+
+load_dotenv()
+# current_file = pathlib.Path(__file__).parent.resolve() 
+# sys.path.append(f"{current_file}/..")
+
+# from func.functions import *
 
 if __name__ == "__main__":
 
     client = Client()
     time.sleep(5)
-    etl(["SHIBUSDT"], client=client)
+    etl(client=client)
