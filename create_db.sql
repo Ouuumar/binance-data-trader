a2577cd82f4dd86506161516008ced3f313e6b1d
Ouuumar <sk8pro@hotmail.fr>
2023-10-21 22:07:32 +0200

    feat(airflow): add custom ariflow image

--- create_db.sql
+++ create_db.sql
@@ -10,9 +10,6 @@ CREATE TABLE IF NOT EXISTS historical_klines(
                 close FLOAT,
                 volume FLOAT,
                 close_time DATETIME,
-                quote_asset_volume FLOAT,
                 number_of_trades BIGINT,
-                taker_buy_base_asset_volume FLOAT,
-                taker_buy_quote_asset_volume FLOAT,
                 symbol VARCHAR(12) NOT NULL,
                 PRIMARY KEY (open_time, symbol));
