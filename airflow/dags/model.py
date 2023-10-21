153acaa414fa09177a8e772fdee73909088131c0
Ouuumar <sk8pro@hotmail.fr>
2023-10-21 22:07:04 +0200

    chore(ml): update ml model

--- airflow/dags/model.py
+++ airflow/dags/model.py
@@ -1,13 +1,10 @@
 from sklearn.model_selection import train_test_split
 from airflow.operators.python import PythonOperator
-from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import MinMaxScaler
 from airflow.utils.dates import days_ago
 from sklearn.metrics import r2_score
-from airflow.models import DagModel
 from datetime import timedelta
-from binance import Client
 from datetime import date
 from airflow import DAG
 import pandas as pd
@@ -37,19 +34,35 @@ args = {
 
 def ml_process(task_instance):
     conn = create_con(user=os.environ["MYSQL_USER"], pw=os.environ["MYSQL_PASSWORD"], ip=os.environ["MYSQL_IP"],port=os.environ["MYSQL_PORT"], db=os.environ["MYSQL_DATABASE"])
+    
     df = pd.read_sql("SELECT * FROM historical_klines", con=conn)
+    df = df.set_index("close_time")
+    df["day"] = df.index.dayofweek
+    df["dayofweek"] = df.index.day_of_week
+    df["quarter"] = df.index.quarter
+    df["month"] = df.index.month
+    df["year"] = df.index.year
+    df["dayofyear"] = df.index.day_of_year
+    
     logging.info(df.head())
-    X = df[["symbol","high", "low", "close", "volume", "quote_asset_volume", "taker_buy_base_asset_volume", "taker_buy_quote_asset_volume"]]
+
+    X = df[["symbol","high", "low", "open", "volume", "number_of_trades", "day", "dayofweek", "quarter", "month", "year", "dayofyear"]]
     X = pd.get_dummies(X, columns=["symbol"])
+    y = df[["close"]]
+    X = X*1
+
     logging.info(X.head())
-    y = df[["open"]]
+    logging.info(y.head())
+    
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
     scaler = MinMaxScaler()
     X_train_scaled = scaler.fit_transform(X_train)
     X_test_scaled = scaler.transform(X_test)
+    
     model = LinearRegression()
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
+    model.fit(X_train_scaled, y_train)
+    y_pred = model.predict(X_test_scaled)
+   
     r2 = r2_score(y_test, y_pred)
     logging.info("Coefficient of determination: %.2f" % r2)
 
